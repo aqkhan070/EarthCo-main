9227e3b048638c5493b6821c85a59b297d5e0e1d
HannanShehzad <Hannan.shehzad201999@gmail.com>
2023-10-11 23:25:44 +0500

    Revert "Update DashboardPage.js"
    This reverts commit d83bb44830d08d1dff3dd35d8cc2beea04a3c614.

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -38,16 +38,9 @@ import AddStaff from '../components/Staff/AddStaff';
 
 const DashboardPage = () => {
   return (
-<<<<<<< HEAD
     <>
       <HeaderExp />
       <SideBar />
-=======
-    <div></div>
-    // <>
-    //   {/* <HeaderExp /> */}
-    //   <SideBar />
->>>>>>> parent of 091317c (dashboard fix)
 
       <div className="content-body" id='contentBody'>
         <Routes>
