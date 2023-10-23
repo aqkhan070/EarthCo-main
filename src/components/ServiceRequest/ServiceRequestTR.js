5799332f25014a7af5629269909eda2be4af07e4
HannanShehzad <Hannan.shehzad201999@gmail.com>
2023-10-23 19:10:59 +0500

    service table

--- src/components/ServiceRequest/ServiceRequestTR.js
+++ src/components/ServiceRequest/ServiceRequestTR.js
@@ -1,61 +1,154 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Link } from "react-router-dom";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel,
+  Button,
+  TablePagination,
+  Checkbox,
+} from "@mui/material";
+import { Create, Delete } from "@mui/icons-material";
 
-const ServiceRequestTR = ({ record, onClick, index }) => {
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#7c9c3d",
+    },
+  },
+});
 
+const ServiceRequestTR = ({ serviceRequest }) => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [sorting, setSorting] = useState({ field: "", order: "" });
 
-    const handleClick = () => {
-        onClick();
+  const sortedCustomers = [...serviceRequest].sort((a, b) => {
+    if (sorting.order === "asc") {
+      return a[sorting.field] > b[sorting.field] ? 1 : -1;
+    } else if (sorting.order === "desc") {
+      return a[sorting.field] < b[sorting.field] ? 1 : -1;
     }
-    return (
-        <tr className='serviceRequestRecords'>
-            <td>
-                {index + 1}
-            </td>
+    return 0;
+  });
 
-            {/* <td>
-                <span className="badge light badge-light">
-                    {record.type}
-                </span>
-            </td> */}
-            <td onClick={handleClick}>
-                <NavLink to={`service-request${record.ID}`} style={{ display: 'contents' }}>
-                    {record.ID}
-                </NavLink>
-            </td>
+  const deleteCustomer = async (id) => {
+    // ... (This part remains unchanged)
+  }
 
-            <td>
-                <p>{record.customer}</p>
-            </td>
-            <td>
-                <p>{record.assign}</p>
-            </td>
-            <td>
-                <p>{record.status}</p>
-            </td>
-            <td>
-                <p>{record.workRequested}</p>
-            </td>
-            <td>
-                <p>{record.created}</p>
-            </td>
-            <td>
-                <p>{record.proposalNo}</p>
-            </td>
-            <td>
-                <div className="flex-box">
-                    <button title="Edit" type="button" className="btn btn-warning btn-sm mx-1">
-                        <i className="fa fa-pen"></i>
-                    </button>
+  const handleDelete = (id) => {
+    if (window.confirm("Are you sure you want to delete this customer?")) {
+      deleteCustomer(id);
+    }
+  }
 
-                    <button title="Delete" type="button" className="btn btn-danger btn-sm mx-1">
-                        <i className="fa fa-trash"></i>
-                    </button>
-                </div>
-            </td>
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="container">
+        <div className="container text-center">
+          <div className="row justify-content-end">
+            <div className="col-3 add-customer-btn">
+              <Link>
+                <Button variant="contained" color="primary">
+                  + Add Customer
+                </Button>
+              </Link>
+            </div>
+          </div>
+          <br />
+          <Table>
+            <TableHead>
+              <TableRow className="table-header">
+                {[
+                  "Select",
+                  "Service Request #",
+                  "Customer Name",
+                  "Assigned to",
+                  "Status",
+                  "Work Requested",
+                  "Date Created",
+                  "Actions",
+                ].map((column, index) => (
+                  <TableCell key={index}>
+                    {index < 5 ? (
+                      <TableSortLabel
+                        active={sorting.field === column}
+                        direction={sorting.order}
+                        onClick={() =>
+                          setSorting({
+                            field: column,
+                            order:
+                              sorting.order === "asc" && sorting.field === column
+                                ? "desc"
+                                : "asc",
+                          })
+                        }
+                      >
+                        {column}
+                      </TableSortLabel>
+                    ) : (
+                      column
+                    )}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {sortedCustomers
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((customer, rowIndex) => (
+                  <TableRow key={rowIndex} hover>
+                    <TableCell>
+                      <Checkbox /> 
+                    </TableCell>
+                    <TableCell>{customer.ServiceRequestNumber}</TableCell>
+                    <TableCell>{customer.CustomerId}</TableCell>
+                    <TableCell>{customer.Assign}</TableCell>
+                    <TableCell>{customer.SRStatusId}</TableCell>
+                    <TableCell>{customer.WorkRequest}</TableCell>
+                    <TableCell>{customer.CreatedDate}</TableCell>
+                    <TableCell>
+                      <Link>
+                        <Button
+                          className="delete-button"
+                          onClick={() => {
+                            // Update actions as required
+                          }}
+                        >
+                          <Create />
+                        </Button>
+                      </Link>
+                      <Button
+                        color="error"
+                        className="delete-button"
+                        onClick={() => handleDelete(customer.CustomerId)}
+                      >
+                        <Delete />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+          <TablePagination
+            component="div"
+            count={sortedCustomers.length}
+            page={page}
+            onPageChange={(event, newPage) => setPage(newPage)}
+            rowsPerPage={rowsPerPage}
+            onRowsPerPageChange={(event) => {
+              setRowsPerPage(parseInt(event.target.value, 10));
+              setPage(0);
+            }}
+          />
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+};
 
-        </tr>
-    )
-}
-
-export default ServiceRequestTR
+export default ServiceRequestTR;
